5fca20d87248c93dc2173f451d9e22a852660da1
Hockenba <github@opalpine.com>
2026-08-10 13:50:03 -0400

    Fix test

--- spec/06-revocation-1_spec.lua
+++ spec/06-revocation-1_spec.lua
@@ -89,12 +89,10 @@ for _, st in ipairs({
       end)
 
       it("SET: stores revocation mark and GET observes it", function()
-        local ok, err = store:set(name, key, mark, long_ttl, current_time)
+        local ok = store:set(name, key, mark, long_ttl, current_time)
         assert.is_not_nil(ok)
-        assert.is_nil(err)
 
-        local data
-        data, err = store:get(name, key, current_time)
+        local data, err = store:get(name, key, current_time)
         assert.is_nil(err)
         assert.equals(mark, data)
       end)
@@ -106,12 +104,10 @@ for _, st in ipairs({
       end)
 
       it("SET: ttl expires revocation entry", function()
-        local ok, err = store:set(name, key2, mark, short_ttl, current_time)
+        local ok = store:set(name, key2, mark, short_ttl, current_time)
         assert.is_not_nil(ok)
-        assert.is_nil(err)
 
-        local data
-        data, err = store:get(name, key2, current_time)
+        local data, err = store:get(name, key2, current_time)
         assert.is_nil(err)
         assert.equals(mark, data)
 
@@ -123,20 +119,17 @@ for _, st in ipairs({
       end)
 
       it("SET: re-mark refreshes ttl", function()
-        local ok, err = store:set(name, key, mark, short_ttl, current_time)
+        local ok = store:set(name, key, mark, short_ttl, current_time)
         assert.is_not_nil(ok)
-        assert.is_nil(err)
 
         sleep(1)
 
-        ok, err = store:set(name, key, mark, long_ttl, time())
+        ok = store:set(name, key, mark, long_ttl, time())
         assert.is_not_nil(ok)
-        assert.is_nil(err)
 
         sleep(short_ttl + 1)
 
-        local data
-        data, err = store:get(name, key, time())
+        local data, err = store:get(name, key, time())
         assert.is_nil(err)
         assert.equals(mark, data)
       end)
